ebbe6c894bafc5dca5193ae32dc1aeb66913aac6
Jonathan Ro <jonro.2955@gmail.com>
2021-03-10 13:04:26 -0800

    localStorage issue test

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function clearTable(){
   }
 }
 
+//makes the creator invisible and resets its fields
 function closeCreator(){
   document.getElementById("newTitle").value = "";
   document.getElementById("newAuthor").value = "";
@@ -38,9 +39,10 @@ function closeCreator(){
   document.querySelector("#bookCreator").style.display = "none";
 }
 
-//a function that detects whether localStorage is supported and available
-function storageAvailable(type) {
-  var storage;
+//a function that returns true if localStorage is supported and available
+function storageAvailable(type) { 
+  //arguments for type can be 'localStorage' or 'sessionStorage'
+  let storage;
   try {
       storage = window[type];
       var x = '__storage_test__';
@@ -77,75 +79,84 @@ addBtn.addEventListener("click", () => {
   }
 });
 
-//creatorCloseBtn makes the creator invisible and resets its fields
 const creatorCloseBtn = document.querySelector("#creatorCloseBtn");
 creatorCloseBtn.addEventListener("click", () => {
   closeCreator();  
 });
 
-
-/*when the book creator's submit button is clicked, create a book using
+/*book creator's submit button: when clicked, create a book using
 the input values, push it into the array, and update the storage*/
 const submitBtn = document.querySelector("#submitBtn");
 submitBtn.addEventListener("click", () => {
-  let newTitle = document.getElementById("newTitle").value; //title: string
-  let newAuthor = document.getElementById("newAuthor").value; //author: string
-  let newPages = document.getElementById("newPages").value; //pages: number
-  let newLink = document.getElementById("newLink").value; //url: string
-  let readStatus; //read status: boolean
+  let newTitle = document.getElementById("newTitle").value; //string
+  let newAuthor = document.getElementById("newAuthor").value; //string
+  let newPages = document.getElementById("newPages").value; //number
+  let newURL = document.getElementById("newURL").value; //string
+  let newStatus; //boolean
   if (document.getElementById("newStatus").checked){
-    readStatus = true;
+    newStatus = true;
   } else {
-    readStatus = false;
+    newStatus = false;
   }
   //only create if the first 2 fields are filled
   if(newTitle != "" && newAuthor != ""){
-    let newBook = new Book(newTitle, newAuthor, newPages, newLink, readStatus);
+    let newBook = new Book(newTitle, newAuthor, newPages, newURL, newStatus);
     myLibrary.push(newBook);
-    updateLocalStorage();
-    updateDisplay();
+    updateLocalStorage(); //updateLocalStorage anytime myLibrary changes
+    updateDisplay(); //updateDisplay anytime local storage is updated
     closeCreator();
   } else {
     alert("Please fill out the 'Title' and 'Author' fields to continue.")
   }
 });
 
-function updateDisplay() {
-  //first, clear all existing elements in the table 
+//updateLocalStorage: first clear it, then update based on myLibrary[]
+function updateLocalStorage(){
+  localStorage.clear();
+  myLibrary.forEach((book, index) => {
+    localStorage.setItem(`book${index}-title`, book.title);
+    localStorage.setItem(`book${index}-author`, book.author);
+    localStorage.setItem(`book${index}-pages`, book.pages);
+    localStorage.setItem(`book${index}-url`, book.url);
+    localStorage.setItem(`book${index}-read`, book.read);
+  });
+}
+
+function updateDisplay(){
   clearTable();
-  //for each book in myLibrary with index i, do the following:
-  myLibrary.forEach((book, i) => {
-    /*Create a container div for each book and append it to the table. This is to achieve 
-    proper appearance in html. The book's details will go inside as separate divs*/
+  //for each book bk in myLibrary with index i, do the following:
+  myLibrary.forEach((bk, i) => {
+    /*Create a container div for each book and append it to the table to achieve 
+    proper appearance in html. Details will go inside as sub-divs*/
     const newBookDiv = document.createElement("div");
     newBookDiv.setAttribute("class", "book");
     table.appendChild(newBookDiv);   
     //create a sub div for the title and append it to the newBookDiv
     const newTitleDiv = document.createElement("div");
-    newTitleDiv.textContent = localStorage.getItem(`book${i}-title`); /*from localStorage*/
     newTitleDiv.setAttribute("class", "attribute");
     newTitleDiv.setAttribute("id", `book${i}-title`);
+    newTitleDiv.textContent = localStorage.getItem(`book${i}-title`); /*from localStorage*/
     newBookDiv.appendChild(newTitleDiv);
     //create a sub div for the author and append it to the newBookDiv
     const newAuthorDiv = document.createElement("div");
-    newAuthorDiv.textContent = localStorage.getItem(`book${i}-author`); /*from localStorage*/
     newAuthorDiv.setAttribute("class", "attribute");
     newAuthorDiv.setAttribute("id", `book${i}-author`);
+    newAuthorDiv.textContent = localStorage.getItem(`book${i}-author`); /*from localStorage*/
     newBookDiv.appendChild(newAuthorDiv);
     //create a sub div for the pages and append it to the newBookDiv
     const newPagesDiv = document.createElement("div");
-    newPagesDiv.textContent = localStorage.getItem(`book${i}-pages`);  /*from localStorage*/
     newPagesDiv.setAttribute("class", "attribute");
     newPagesDiv.setAttribute("id", `book${i}-pages`);
+    newPagesDiv.textContent = localStorage.getItem(`book${i}-pages`); /*from localStorage*/
     newBookDiv.appendChild(newPagesDiv);
     //create a sub div for the URL and append it to the newBookDiv
     const newUrlDiv = document.createElement("div");
+    newUrlDiv.setAttribute("class", "attribute");
+    newUrlDiv.setAttribute("id", `book${i}-url`);
     let myURL = localStorage.getItem(`book${i}-url`); /*from localStorage*/
     if (myURL != ""){
       newUrlDiv.innerHTML = `<a target="blank" href=${myURL}>${myURL}</a>`;  
     }
-    newUrlDiv.setAttribute("class", "attribute");
-    newUrlDiv.setAttribute("id", `book${i}-url`);
     newBookDiv.appendChild(newUrlDiv);
     //append a read status button to newBookDiv
     const readBtn = document.createElement("button");
@@ -160,7 +171,7 @@ function updateDisplay() {
     readBtn.addEventListener("click", () => {
       /*anytime a myLibrary object is changed or deleted, update the the local storage 
       and update the display*/
-      book.toggleRead();
+      bk.toggleRead();
       updateLocalStorage();
       updateDisplay();
     });
@@ -169,38 +180,14 @@ function updateDisplay() {
     newBookDiv.appendChild(deleteBtn);  
     deleteBtn.textContent = "Delete";
     deleteBtn.setAttribute("class", "bookEntryBtns");
-    //associate an array index number to each delete button
+    //associate a data-index attribute to each delete button
     deleteBtn.setAttribute("data-index", i); 
     deleteBtn.addEventListener("click", () => {
-      //"deleteBtn.dataset.index" returns the data-index value of this deleteBtn
-      myLibrary.splice(deleteBtn.dataset.index, 1); //splice to remove 1 book at index
+      //"deleteBtn.dataset.index" returns the data-index value of this element
+      myLibrary.splice(deleteBtn.dataset.index, 1); //removes one book at index
       updateLocalStorage();
       updateDisplay();
     });
   });
 }
-
-/*create a function that goes through each book in myLibrary and saves to localStorage
-the id-value pairs of each book's attibute 
-*/
-function updateLocalStorage(){
-  localStorage.clear();
-  myLibrary.forEach((book, index) => {
-    localStorage.setItem(`book${index}-title`, book.title);
-    localStorage.setItem(`book${index}-author`, book.author);
-    localStorage.setItem(`book${index}-pages`, book.pages);
-    localStorage.setItem(`book${index}-url`, book.url);
-    localStorage.setItem(`book${index}-read`, book.read);
-  });
-}
-
-//sample books for testing
-let sampleBook0 = new Book("sampleBook0", "Author0", 256, "https://www.amazon.com/", true);
-let sampleBook1 = new Book("sampleBook1", "Author1", 321, "https://www.amazon.com/", false);
-let sampleBook2 = new Book("sampleBook2", "Author2", 456, "https://www.barnesandnoble.com/", false);
-myLibrary.push(sampleBook0);
-myLibrary.push(sampleBook1);
-myLibrary.push(sampleBook2);
-updateLocalStorage();
-updateDisplay();
 
